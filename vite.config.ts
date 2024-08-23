ae59f6d747ff02b766c15f70cc7e036b0f380d1c
Nikita <nikita.shkuratt@gmail.com>
2024-08-23 22:27:08 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import path from 'path'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tsconfigPaths(),
+
+  ],
+  server: {
+    port: 3000,
+    open: true,
+    host: true
+  },
+  esbuild: {
+    loader: 'tsx'
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src/')
+    }
+  }
+})
