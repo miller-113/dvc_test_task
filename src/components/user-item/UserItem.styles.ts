8677f97bfcb4c9c5bdb9c116c14089f74f3f8ed3
Nikita <nikita.shkuratt@gmail.com>
2024-08-26 01:59:09 +0300

    added user item Component to table

---
+++ src/components/user-item/UserItem.styles.ts
@@ -0,0 +1,25 @@
+export const styles = {
+  cell: {
+    p: '18px 10px 22px',
+    border: 'unset'
+  },
+  firstLastItemCell: {
+    p: '18px 36px 22px'
+  },
+  fullNameCell: {
+    fontFamily: 'Rubik, sans-serif',
+    fontWeight: '400',
+    color: 'rgba(27, 36, 56, 1)',
+    fontSize: '14px',
+    letterSpacing: '0.2px',
+    lineHeight: '20px'
+  },
+  additionalCell: {
+    fontFamily: 'Rubik, sans-serif',
+    fontWeight: '300',
+    color: 'rgba(94, 98, 107, 1)',
+    fontSize: '14px',
+    letterSpacing: '0.2px',
+    lineHeight: '20px'
+  }
+}
